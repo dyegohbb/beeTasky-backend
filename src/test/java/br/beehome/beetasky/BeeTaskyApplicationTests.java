c55605f1913790743287110c385f2c20e2644973
Dyego H <dyegohbb@gmail.com>
2025-03-01 15:07:25 -0300

    :sparkles: feat: project starts

---
+++ src/test/java/br/beehome/beetasky/BeeTaskyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.beehome.beetasky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeTaskyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
